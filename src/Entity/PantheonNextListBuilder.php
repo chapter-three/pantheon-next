a7c888df597b6b041f5c26d6f21a5a08967090da
shadcn <arshad@chummun.com>
2022-08-29 12:00:12 +0400

    refactor: inject form builder service in PantheonNextListBuilder

--- src/Entity/PantheonNextListBuilder.php
+++ src/Entity/PantheonNextListBuilder.php
@@ -4,10 +4,12 @@ namespace Drupal\pantheon_next\Entity;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
-use Drupal\Core\Link;
-use Drupal\Core\Url;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Component\Serialization\Json;
 use Drupal\pantheon_next\Form\PantheonNextInstallerForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a base class to build a listing entities.
@@ -15,6 +17,39 @@ use Drupal\pantheon_next\Form\PantheonNextInstallerForm;
  * @ingroup pantheon_next
  */
 class PantheonNextListBuilder extends EntityListBuilder {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected FormBuilderInterface $formBuilder;
+
+  /**
+   * Constructs a new PantheonNextListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder service.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, FormBuilderInterface $form_builder) {
+    parent::__construct($entity_type, $storage);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('form_builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -38,7 +73,8 @@ class PantheonNextListBuilder extends EntityListBuilder {
       ],
       'query' => \Drupal::destination()->getAsArray(),
     ];
-    $row['site'] = $entity->getNextSite()->toLink($entity->getNextSite()->label(), 'edit-form', $options);
+    $row['site'] = $entity->getNextSite()->toLink($entity->getNextSite()
+      ->label(), 'edit-form', $options);
     $row['base_url'] = $entity->getNextSite()->getBaseUrl();
     $row['consumer'] = ($consumer = $entity->getConsumer()) ? $consumer->toLink($consumer->label(), 'edit-form', $options) : $this->t('Error: Not specified');
     return $row + parent::buildRow($entity);
@@ -54,9 +90,8 @@ class PantheonNextListBuilder extends EntityListBuilder {
       return $build;
     }
 
-    // Replace the list with a wizard.
-    // TODO: Inject this.
-    return \Drupal::formBuilder()->getForm(PantheonNextInstallerForm::class);
+    // Show the installer form if no connection has been created.
+    return $this->formBuilder->getForm(PantheonNextInstallerForm::class);
   }
 
   /**
